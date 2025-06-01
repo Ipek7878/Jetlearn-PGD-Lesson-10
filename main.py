91b86246c16f760859af6f5c72242ea5306624c9
Ipek7878 <i.boyaci@ghostrobotics.nl>
2025-06-01 11:41:37 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ def draw():
             item.draw()
 
 def display_message(heading,sub_heading):
-    screen.draw.text(heading,fontsize=60,center=(400,300), color="black")
-    screen.draw.text(sub_heading,fontsize=50,center=(400,250),color="black")
+    screen.draw.text(heading,fontsize=60,center=(400,250), color="black")
+    screen.draw.text(sub_heading,fontsize=50,center=(400,300),color="black")
 
 def update():
     global items
@@ -69,7 +69,7 @@ def get_option_to_create(number_of_extra_items):
         random_option=random.choice(ITEMS)
         items_to_create.append(random_option)
     return items_to_create
-
+ 
 def create_items(items_to_create):
     new_items=[]
     for i in items_to_create:
@@ -78,11 +78,51 @@ def create_items(items_to_create):
     return new_items
 
 def layout_items(items_to_layout):
-    pass
+    number_of_gaps= len(items_to_layout) + 1
+    gap_size= WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+    for index,item in enumerate(items_to_layout):
+        new_x_pos = (index +1) * gap_size
+        item.x= new_x_pos
+
 
 def animate_items(items_to_animate):
-    pass
+    global animations
+    for items in items_to_animate:
+        duration= START_SPEED - current_level
+        animation= animate(items,duration=duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over=True
+
+
+#on_mouse_down() is an in-built function that checks if a key is clicked or not
+def on_mouse_down(pos):
+    global items
+    for item in items:
+        if item.collidepoint(pos):
+            if 'paper' in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    
+    if current_level== FINAL_LEVEL:
+        game_complete= True
+    else:
+        current_level=current_level+1
+        items=[]
+        animations=[]
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
 
 
 pgzrun.go()
-
